50e008138f8d943d744be441ce8c584833f836d7
caiwang <caiwang@tsinghua.org.cn>
2014-12-15 00:09:25 +0800

    new table useractive for iserver / modify column userid

--- db/upd4img1121-modify.sql
+++ db/upd4img1121-modify.sql
@@ -13,6 +13,7 @@ ALTER TABLE useraccounts ADD COLUMN `captcha` varchar(10) DEFAULT NULL;
 # add column pushflag in usermacs table
 ALTER TABLE usermacs ADD COLUMN `pushflag` tinyint DEFAULT '1';
 
+
 # add columns in useractive table
 ALTER TABLE useractive ADD COLUMN `srcip` varchar(64) DEFAULT NULL;
 ALTER TABLE useractive ADD COLUMN `sender` varchar(36) DEFAULT NULL;
@@ -21,3 +22,12 @@ ALTER TABLE useractive ADD COLUMN `progid` varchar(36) DEFAULT NULL;
 ALTER TABLE useractive ADD COLUMN `updtime` datetime DEFAULT NULL;
 ALTER TABLE useractive ADD COLUMN `pushflag` tinyint DEFAULT '1';
 ALTER TABLE useractive ADD COLUMN `srcid` int DEFAULT NULL;
+
+
+# modify userid
+ALTER TABLE useraccounts MODIFY COLUMN `userid` varchar(36) DEFAULT NULL;
+ALTER TABLE usermacs MODIFY COLUMN `userid` varchar(36) DEFAULT NULL;
+ALTER TABLE useractive MODIFY COLUMN `userid` varchar(36) DEFAULT NULL;
+ALTER TABLE userpoints MODIFY COLUMN `userid` varchar(36) DEFAULT NULL;
+ALTER TABLE userlog MODIFY COLUMN `userid` varchar(36) DEFAULT NULL;
+
