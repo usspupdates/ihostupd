0b2dc96c99b05f7a5478d6a02d9678e7712895bd
caiwang <caiwang@tsinghua.org.cn>
2014-12-20 18:19:09 +0800

    move iserver.on/off.sh to ihost

--- system/iserver.off.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed  -i  "s|@reboot sudo python /root/wlsp2iserver.py|#@reboot sudo python /root/wlsp2iserver.py|g"  /var/spool/cron/crontabs/root
-sed  -i  "s|@reboot sudo python /root/user2iserver.py|#@reboot sudo python /root/user2iserver.py|g"  /var/spool/cron/crontabs/root
-
-
