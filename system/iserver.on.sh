f6948bedc6229e5f0b1af5c3935a2a6570ace1df
caiwang <caiwang@tsinghua.org.cn>
2014-12-20 18:15:26 +0800

    push to iserver on/off

---
+++ system/iserver.on.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sed  -i  "s|#@reboot sudo python /root/wlsp2iserver.py|@reboot sudo python /root/wlsp2iserver.py|g"  /var/spool/cron/crontabs/root
+sed  -i  "s|#@reboot sudo python /root/user2iserver.py|@reboot sudo python /root/user2iserver.py|g"  /var/spool/cron/crontabs/root
+
+
